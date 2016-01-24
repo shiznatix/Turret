fcc3535d59d571bb627226eb5e781f7fb5772f3c
shiznatix <git@shiznatix.com>
2016-01-24 03:51:32 +0200

    Moved files

---
+++ Android/app/src/main/java/com/shiznatix/turret/Constants.java
@@ -0,0 +1,15 @@
+package com.shiznatix.turret;
+
+public interface Constants {
+    public static final String TAG_PREFIX = "tur.";
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "deviceName";
+    public static final String TOAST = "toast";
+}
